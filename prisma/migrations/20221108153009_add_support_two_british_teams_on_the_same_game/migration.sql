dd917efb84248ea7681a7c09a402556c465d662c
Claudio Ferreira <claudio.ferreira@aizoon.it>
2022-11-18 18:04:00 +0100

    nlw server first commit

---
+++ prisma/migrations/20221108153009_add_support_two_british_teams_on_the_same_game/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `britishCountry` on the `Game` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Game" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "date" DATETIME NOT NULL,
+    "firstTeamCountryCode" TEXT NOT NULL,
+    "secondTeamCountryCode" TEXT NOT NULL,
+    "firstGBCountry" TEXT,
+    "secondGBCountry" TEXT
+);
+INSERT INTO "new_Game" ("date", "firstTeamCountryCode", "id", "secondTeamCountryCode") SELECT "date", "firstTeamCountryCode", "id", "secondTeamCountryCode" FROM "Game";
+DROP TABLE "Game";
+ALTER TABLE "new_Game" RENAME TO "Game";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
